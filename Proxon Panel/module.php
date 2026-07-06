68b45d54ebc04faf2c6d730b5ef38c1bb622ee9b
MiGo <35539493+migodev@users.noreply.github.com>
2026-07-06 11:19:26 +0200

    CHanged Prefix

--- Proxon Panel/module.php
+++ Proxon Panel/module.php
@@ -7,7 +7,7 @@
 			$this->RegisterPropertyInteger("ControlPanel", 1);
 			$this->RegisterPropertyInteger("Interval", 30);
 
-			$this->RegisterTimer("Poller", 0, "PROXON_FWT1_RequestStatus(\$_IPS['TARGET']);");
+			$this->RegisterTimer("Poller", 0, "PROXONFWT1_RequestStatus(\$_IPS['TARGET']);");
  
 		}
 
