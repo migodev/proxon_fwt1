f4f40c7d9405bd2e44b6a75314fffd119752ecc7
MiGo <35539493+migodev@users.noreply.github.com>
2026-07-10 14:59:32 +0200

    Update module.php

--- Proxon Panel/module.php
+++ Proxon Panel/module.php
@@ -90,10 +90,10 @@
 			// Only for Panels > 1
 			if ($this->ReadPropertyInteger("ControlPanel") > 1) {
 				$baseTemp = $this->readTemperature(220, false, 10.0, 2);				
-$baseTemp = 5;
+
 				// Read last value of BaseTemperature
 				$oldBaseTemp = $this->ReadAttributeFloat("BaseTemperature");
-
+$oldBaseTemp = 5;
 				// Edit Presentation only on Change
 				if ($baseTemp != $oldBaseTemp) {
 					// We want to store the BaseTemperature in a attribute, to use it for SetTemperature / comparison
